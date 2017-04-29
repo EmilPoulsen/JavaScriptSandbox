25d3fe23490742bcb14c86ee91a472b8dcd571e6
Emil Poulsen <emil-poulsen@hotmail.com>
2017-04-29 02:11:58 -0400

    added html

--- Node/01_Example/app.js
+++ Node/01_Example/app.js
@@ -1,5 +1,22 @@
-function greeting(greet){
-  console.log(greet);
-}
+const http = require('http');
+const fs = require('fs');
 
-greeting('hello world');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+fs.readFile('index.html', (err, html) => {
+  if (err) {
+    throw err;
+  }
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/html');
+    res.write(html);
+    res.end();
+    //res.end('Hello World');
+  });
+
+  server.listen(port, hostname, () => {
+    console.log('Server started on port ' + port);
+  });
+});
