180ff8c6d29ef99fbde26022fc986c0b5f300976
Emil Poulsen <emil-poulsen@hotmail.com>
2017-05-29 15:35:44 -0400

    added people json. to be deleted

--- YoutubeTutorials/ExpressJS_CrashCourse/app.js
+++ YoutubeTutorials/ExpressJS_CrashCourse/app.js
@@ -15,18 +15,34 @@ app.use(logger); //middle ware. runs everytime when application is loaded
 
 // Body Parser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 //place to put static resourses.. jquerty, css
 app.use(express.static(path.join(__dirname, 'public')));
 
+var people = [
+  {
+  name: 'Jeff',
+  age: 30
+},
+  {
+  name: 'Sara',
+  age: 33
+},
+  {
+  name: 'Bill',
+  age: 55
+}
+]
 
-app.get('/', function(req,res){ //define a route
-  res.send('Hello World');
+app.get('/', function(req, res) { //define a route
+  res.json(people);
 })
 
 //app.use(logger); dont put here
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log('server started on port 3000...');
 })
