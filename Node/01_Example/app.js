1081c47f8746b726d3f3638c9c16e81ca0d914db
Emil Poulsen <emil-poulsen@hotmail.com>
2017-05-29 14:22:19 -0400

    documentation. last before moving on to express

--- Node/01_Example/app.js
+++ Node/01_Example/app.js
@@ -1,16 +1,17 @@
-const http = require('http');
-const fs = require('fs');
+//importing module
+const http = require('http'); //core module
+const fs = require('fs'); //file system load in..
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '127.0.0.1'; //out local host
+const port = 3000; //
 
-fs.readFile('index.html', (err, html) => {
+fs.readFile('index.html', (err, html) => { //call back.. arrow function
   if (err) {
     throw err;
   }
-  const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/html');
+  const server = http.createServer((req, res) => { //create a function
+    res.statusCode = 200; //everything is ok
+    res.setHeader('Content-type', 'text/html'); //set context type to text plain html
     res.write(html);
     res.end();
     //res.end('Hello World');
