ad85768d70d33bcbfd60403414c8c0963a703c4b
Emil Poulsen <emil-poulsen@hotmail.com>
2017-12-28 15:17:48 -0500

    progress on connecting mongodb

--- Tutorials/mean_stack/server.js
+++ Tutorials/mean_stack/server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var Post = require('./models/post')
 
 var app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 app.get('/api/posts', function(req, res) {
@@ -9,6 +13,17 @@ app.get('/api/posts', function(req, res) {
     username: 'dickeyxxx',
     body: 'node rocks!'
   }]);
+});
+
+app.post('/api/posts', function(req, res){
+  //console.log(req.body);
+  console.log('post received!');
+  console.log(req.body.username);
+  console.log(req.body.body);
+
+
+
+  res.sendStatus(201);
 });
 
 app.listen(3000, function(){
