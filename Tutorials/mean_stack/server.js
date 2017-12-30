446bd55e543852a83dcae01be0e277bcb0512d51
Emil Poulsen <emil-poulsen@hotmail.com>
2017-12-30 11:48:10 -0500

    updated get req with db code

--- Tutorials/mean_stack/server.js
+++ Tutorials/mean_stack/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var Post = require('./models/post');
 
 var app = express();
 app.use(bodyParser.urlencoded({
@@ -7,14 +8,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.get('/api/posts', function(req, res) {
-  res.json([{
-    username: 'dickeyxxx',
-    body: 'node rocks!'
-  }]);
+app.get('/api/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err) {return next(err)}
+    res.json(posts);
+  });
 });
 
-var Post = require('./models/post')
 app.post('/api/posts', function(req, res){
   //console.log(req.body);
   console.log('post received!');
